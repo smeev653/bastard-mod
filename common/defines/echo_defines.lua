9cdc11b6fbab346155467fde064d867faa4929bb
smeev653 <smeev605@gmail.com>
2024-01-04 13:57:32 +0000

    tariffs closer to vanilla

--- common/defines/echo_defines.lua
+++ common/defines/echo_defines.lua
@@ -86,12 +86,9 @@ NDefines.NCountry.INNOVATIVENESS_AHEAD_OF_TIME_BONUS = 0.005
 NDefines.NCountry.INNOVATIVENESS_FIRST_REACHED_TECH_LEVEL = 4.0  -- Innovativeness bonus to first country (or countries) to reach a certain tech level.
 NDefines.NCountry.INNOVATIVENESS_FIRST_PICKED_IDEA = 2.0			-- Innovativeness bonus to first country (or countries) to pick an idea.																		
 NDefines.NCountry.SCRIPTED_ADVISOR_DISCOUNT = 0.75				-- Multiplier on cost for advisors scripted with discount=yes
-NDefines.NCountry.PS_RAISE_TARIFFS = 25
 NDefines.NCountry.PS_LOWER_TARIFFS = 10
-NDefines.NCountry.BASE_TARIFF = 0.15								-- Basic part of colonies income that goes to tariffs
-NDefines.NCountry.TARIFF_INCREASE_STEP = 0.1					-- Increase on each boost
+NDefines.NCountry.PS_RAISE_TARIFFS = 25
 NDefines.NCountry.TARIFF_LIBERTY_INCREASE = 0.5					-- Liberty increase for each % tariffs
-NDefines.NCountry.TARIFF_DECREASE_STEP = -0.1
 NDefines.NCountry.MINIMUM_POLICY_TIME = 1
 NDefines.NCountry.ESTATE_MIN_DISTRIBUTED_CROWNLAND = 30.5
 NDefines.NCountry.ESTATE_DEFAULT_LOYALTY = 50
